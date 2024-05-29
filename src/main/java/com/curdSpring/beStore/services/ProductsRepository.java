d112ce9849b2f89135dec1db2345f27edb048bea
delmount <adakrishna@deloitte.com>
2024-05-29 18:14:39 +0530

    deletedfiles

--- src/main/java/com/curdSpring/beStore/services/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.curdSpring.beStore.services;
-
-import com.curdSpring.beStore.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<Product,Integer> {
-
-}
